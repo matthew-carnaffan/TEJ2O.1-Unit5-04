ed79d9174c3504bb3bbe277b74bedb26e547d992
matthew-carnaffan <matthew.carnaffan@stu.ocsb.ca>
2026-03-04 16:58:59 -0500

    this code runs the traffic light program but has a error

--- main.ts
+++ main.ts
@@ -5,3 +5,36 @@
  * This program run the colours of a traffic light over 3 neo pixels.
 */
 
+//variables
+let myStrip: neopixel.Strip = null
+
+//clear leds
+myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//traffic light code for led green
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    myStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    pause(2000)
+
+    //traffic light code for led yellow
+    myStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    myStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    pause(3000)
+
+    //traffic light code for led red
+    myStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    myStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    pause(5000)
+
+    //traffic light code for walk
+    myStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    basic.showIcon(IconNames.StickFigure)
+    myStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
+    pause(3000)
+
+    //traffic light code for the end of the cicle
+    myStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+})
