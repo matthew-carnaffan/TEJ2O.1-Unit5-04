6393a134cf6e7a59b1a907116a9183f700d9afc4
matthew-carnaffan <matthew.carnaffan@stu.ocsb.ca>
2026-03-04 12:51:00 -0500

    I filled in required info

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Matthew Carnaffan
+ * Created on: March 2026
+ * This program run the colours of a traffic light over 3 neo pixels.
 */
 
-basic.showString('Hello, World!')
